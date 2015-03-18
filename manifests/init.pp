919ecb006a6e7413609420898a39e31576871ac5
michaelvol <michaelvol@gmail.com>
2015-03-18 13:37:28 +0200

    use mgmt_ip var

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 class guardicore-honeypot {
 
   exec { 'download datapath':
-    command => '/usr/bin/curl --insecure https://10.0.1.184:8000/repos/datapath.tar.gz -o /tmp/datapath.tar.gz',
+    command => '/usr/bin/curl --insecure https://${mgmt_ip}:8000/repos/datapath.tar.gz -o /tmp/datapath.tar.gz',
     logoutput => true,
   }
 
@@ -15,7 +15,7 @@ class guardicore-honeypot {
   exec { 'setup.sh': 
     #path => '/var/lib/guardicore/datapath/scripts/openstack/Havana',
     #command => "setup.sh install 127.0.0.1 --local",
-    command => "/var/lib/guardicore/datapath/scripts/openstack/Havana/setup.sh install 10.0.1.184 --local",
+    command => "/var/lib/guardicore/datapath/scripts/openstack/Havana/setup.sh install ${mgmt_ip} --local",
     timeout => 0,
     require => Exec["extract datapath"],
     logoutput => true,
